577f58bb158806348405a47daae76d076e61f8bc
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-13 21:46:39 -0400

    version1

---
+++ src/PigLatin.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PigLatin {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);	
+		System.out.println("Welcome to the Pig Latin Translator! Roll the dice? (yes/no):");
+		
+		
+
+
+		}
+	}
+
+
