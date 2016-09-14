2b3e5cc3f29b31ade0c479814074a01f518a6991
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-13 21:58:50 -0400

    application outline

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 public class PigLatin {
 
 	public static void main(String[] args) {
-	
+	//the application welcomes and prompts the user to enter a word or phrase
+	//the application translates the word entered by user into Pig Latin
+	//the applications asks the user if they want to repeat	
+		
+	//classes that need to be imported	
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Welcome to the Pig Latin Translator! Roll the dice? (yes/no):");
+	
+	//string variables needed
 		
 		
 
-
+	//welcome to user
+		
+	//asking user to enter a word or phrase	
 		}
 	}
 
