9c1ed04f246b955c9663a6b71ca10a14550552e9
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-15 09:19:19 -0400

    updated

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -12,20 +12,34 @@ public class PigLatin {
 		String original;
 		String translated;
 		char firstletter; 
+		
 	//welcomes and asks user to enter a word
 		System.out.println("Welcome to the Pig Latin Translator!");
 		System.out.print("enter a word: ");
-	    original = sc.next();
+	    original = sc.nextLine();
+	    
+	    System.out.println(original.toLowerCase( ) );
+	    
+	    original = original.toLowerCase();
 	    
 	//change entered word into all lowercase characters    
 	//if starts with a vowel add way to the end
-	    
-	    firstletter = original.charAt(0); //
-	    translated = original.substring(1) + firstletter + "ay";  
-	    System.out.println("in pig latin, it's: " + translated);
-			
-		
+	   
+	    if(true){ //update if statement 
+	        //need to figure out build specification 3
+	    }
+	    else {
+			firstletter = original.charAt(0);
+			//takes first character and isolates it if i replaced (0) with (1) it would isolate the second letter 
+			translated = original.substring(1) + firstletter + "ay";
+			//takes off the first letter of the inputed word
+			System.out.println("in pig latin, it's: " + translated);
 		}
+	   
+	   sc.close(); //use to close 	
+	
+		}
+	
 	}
 
 
