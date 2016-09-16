b7d5b04ce33eb9ad3bcf1693dbcabea88b421155
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-15 21:46:22 -0400

    updated

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,50 +1,54 @@
-import java.util.Scanner; //import scanner class to get 
+	/*This application will do three things. 
+	 	* 1. It will and prompts the user to enter a word or phrase
+		* 2. It will translates the word entered by user into Pig Latin
+		* 3. It will ask the user if they want to repeat*/	
+
+	/*This application needs help :(
+	 	* 1. Needs to resolve error on line 39 
+		* 2. Add build specification 3
+		* 3. Add extended features*/	
+
+import java.util.Scanner;
 
 public class PigLatin {
 
 	public static void main(String[] args) {
-	//the application welcomes and prompts the user to enter a word or phrase
-	//the application translates the word entered by user into Pig Latin
-	//the applications asks the user if they want to repeat	
-		
-		
+	
 		Scanner sc = new Scanner(System.in); 
 		String original;
 		String translated;
 		String userCont;
 		char firstletter; 
-		boolean Cont; 
 		
-	//welcomes and asks user to enter a word
-		System.out.println("Welcome to the Pig Latin Translator!");
-		System.out.print("enter a word: ");
+		System.out.println("Welcome to the Pig Latin Translator!");//user greeting
+		System.out.print("enter a word: ");//user inputs a word
 	    original = sc.nextLine();
 	    
 	    System.out.println(original.toLowerCase( ) );
-	    
 	    original = original.toLowerCase();
-	    
-	//change entered word into all lowercase characters    
-	//if starts with a vowel add way to the end
+		
+		//change inputed word into all lower case characters
+		
+		//determine if first letter is a vowel
+		//if first letter is vowel add "say"
+		//else first letter is consonant loop through users inputed word and add "ay" at end
+		
 	    System.out.println("Continue? (y/n)");
-		userCont =input.nextLine();
-	    
-	    if(userCont.equalsIgnoreCase("y")){
-	    	userCont=true); //update if statement 
-	        //need to figure out build specification 3
-	    }
-	    else {
-			firstletter = original.charAt(0);
-			//takes first character and isolates it if i replaced (0) with (1) it would isolate the second letter 
-			translated = original.substring(1) + firstletter + "ay";
-			//takes off the first letter of the inputed word
-			System.out.println("in pig latin, it's: " + translated);
-		}
-	   
-	   sc.close(); //use to close 	
-	
-		}
-	
+		
+		
+		 if(userCont.equalsIgnoreCase("y")){
+		    	
+		    	
+		 }
+		    else {
+				firstletter = original.charAt(0);
+				//takes first character and isolates it if i replaced (0) with (1) it would isolate the second letter 
+				translated = original.substring(1) + firstletter + "ay";
+				//takes off the first letter of the inputed word
+				System.out.println("in pig latin, it's: " + translated);
+			}
+		   
+		   sc.close();
 	}
-
+}
 
